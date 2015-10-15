f98cfccbe585e80114ccce1d3f1ea061797ca475
Pablo <diesire@gmail.com>
2015-10-15 15:37:01 +0200

    Add MarkdownRedux class

--- spec/markdown-redux-spec.js
+++ spec/markdown-redux-spec.js
@@ -3,7 +3,11 @@
 import MarkdownRedux from '../lib/markdown-redux';
 
 describe("MarkdownRedux", function() {
-  return it("has one valid test", function() {
-    return expect(MarkdownRedux).toBe('markdown-redux core loaded');
+  it("is loaded", function() {
+    expect(MarkdownRedux).toBeDefined();
+  });
+
+  it("is a class", function() {
+    expect(new MarkdownRedux()).toBeDefined();
   });
 });
