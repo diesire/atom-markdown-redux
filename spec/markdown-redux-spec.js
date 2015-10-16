7e3dcc1ae7400a4a3f17dca80e97b2e32b8cc0f2
Pablo <diesire@gmail.com>
2015-10-16 11:00:03 +0200

    Add toc method

--- spec/markdown-redux-spec.js
+++ spec/markdown-redux-spec.js
@@ -2,6 +2,27 @@
 
 import MarkdownRedux from '../lib/markdown-redux';
 
+const TEXT_WITH_TOC_01_INPUT =
+`# Table of Contents
+# Title 1
+A paragraph
+# Title 2
+Another paragraph`
+const TEXT_WITH_TOC_01_OUTPUT =
+`# Table of Contents
+
+-   [Title 1](#title-1)
+-   [Title 2](#title-2)
+
+# Title 1
+
+A paragraph
+
+# Title 2
+
+Another paragraph
+`
+
 describe("MarkdownRedux", function() {
   it("is loaded", function() {
     expect(MarkdownRedux).toBeDefined();
@@ -9,5 +30,9 @@ describe("MarkdownRedux", function() {
 
   it("is a class", function() {
     expect(new MarkdownRedux()).toBeDefined();
+  });
+
+  it("has a method", function() {
+    expect(new MarkdownRedux().toc(TEXT_WITH_TOC_01_INPUT)).toEqual(TEXT_WITH_TOC_01_OUTPUT);
   });
 });
