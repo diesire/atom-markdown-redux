11319059601c7dc34ac327c10e1df42d60057959
Pablo <diesire@gmail.com>
2015-10-26 13:40:07 +0100

    Fix TOC header detection

--- spec/markdown-redux-toc-spec.js
+++ spec/markdown-redux-toc-spec.js
@@ -8,6 +8,7 @@ const TEXT_WITH_TOC_01_INPUT =
 A paragraph
 # Title 2
 Another paragraph`
+
 const TEXT_WITH_TOC_01_OUTPUT =
 `# Table of Contents
 
@@ -15,10 +16,52 @@ const TEXT_WITH_TOC_01_OUTPUT =
 -   [Title 2](#title-2)
 `
 
+const TEXT_WITH_TOC_02_INPUT =
+`# table-of-contents
+# Title 1
+A paragraph
+# Title 2
+Another paragraph`
+
+const TEXT_WITH_TOC_02_OUTPUT =
+`# table-of-contents
+
+-   [Title 1](#title-1)
+-   [Title 2](#title-2)
+`
+
+const TEXT_WITH_TOC_03_INPUT =
+`# table-of-contents
+# Title 1
+A paragraph
+# Title 2
+Another paragraph`
+
+const TEXT_WITH_TOC_03_OUTPUT =
+`# table-of-contents
+
+-   [Title 1](#title-1)
+-   [Title 2](#title-2)
+`
+
 describe("MarkdownReduxToc", function() {
-  describe("toc", function() {
-    it("returns a String", function() {
-      expect(new MarkdownReduxToc(TEXT_WITH_TOC_01_INPUT).toc().asString).toEqual(TEXT_WITH_TOC_01_OUTPUT);
+  describe("method toc", function() {
+    describe("when finds the header 'toc'", function() {
+      it("returns a String", function() {
+        expect(new MarkdownReduxToc(TEXT_WITH_TOC_03_INPUT).toc().asString).toEqual(TEXT_WITH_TOC_03_OUTPUT);
+      });
+    });
+
+    describe("when finds the header 'Table of Contents'", function() {
+      it("returns a String", function() {
+        expect(new MarkdownReduxToc(TEXT_WITH_TOC_01_INPUT).toc().asString).toEqual(TEXT_WITH_TOC_01_OUTPUT);
+      });
+    });
+
+    describe("when finds the header 'table-of-contents'", function() {
+      it("returns a String", function() {
+        expect(new MarkdownReduxToc(TEXT_WITH_TOC_02_INPUT).toc().asString).toEqual(TEXT_WITH_TOC_02_OUTPUT);
+      });
     });
   });
 });
